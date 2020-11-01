475b3ec8ae0f28e4606698d55dbcbc5786bf3cf6
Jeril <3026242+jerilj@users.noreply.github.com>
2020-11-01 15:30:13 -0500

    Feature: Added Calculator object initiation test

--- CalculatorTests.py
+++ CalculatorTests.py
@@ -1,9 +1,18 @@
 import unittest
 
+from Calculator import Calculator
+from CSVReader import CSVReader
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
